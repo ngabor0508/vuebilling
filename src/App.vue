01e3d4f483b9fa21f896d6a76b84500433d15177
nagy_gabor <ngabor0508@gmail.com>
2021-12-07 16:08:53 +0100

    Javítások

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Operations</th>
+          <th>Összérték</th>
+        </tr>
+      </thead> 
+      
+      <tbody>
+        <tr v-for="row in rows"
+          v-bind:key="row.title">
+
+          <td>{{ row.title }}</td>
+          <td>{{ row.price }}</td>
+          <td>{{ row.quantity }}</td>
+          <td>
+            <button>X</button>
+            <button>Edit</button>
+          </td>
+          <td>{{ row.price * row.quantity }} </td>
+        </tr>
+
+        <tr>
+          <td><input type="text" name="input_title" id="input_title" placeholder="Title"></td>
+          <td><input type="number" name="input_price" id="input_price" placeholder="Price"></td>
+          <td><input type="number" name="input_quantity" id="input_quantity" placeholder="Quantity"></td>
+          <button name="input_add" id="input_add">Add</button>
+        </tr>
+
+
+
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data() {
     return {
